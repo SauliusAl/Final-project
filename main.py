543874aada6a6fcf4a93cc76a24452a8848fd5f8
gitSaulius <saule777@gmail.com>
2023-07-10 20:28:33 +0300

    Message

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+# Baigiamasis darbas
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from colorama import init, Fore, Back, Style
+
+def load_data1(file):
+    data1 = pd.read_csv(file, encoding='utf-8')
+    # return data
+
+    list_of_years1 = data1['Metai'].to_list()
+    list_of_vma = data1['Vid_mot_amzius'].to_list()
+    list_of_vap = data1['Vid_amz_pirmag'].to_list()
+
+    x = np.array(list_of_years1)
+    y1 = np.array(list_of_vma)
+    y2 = np.array(list_of_vap)
+
+    plt.plot(x, y1, label = 'Visų gimdymų')
+    plt.plot(x, y2, color = 'r', label = 'Pirmagimis')
+    plt.title('Gimdančių moterų vidutinis amžius')
+    plt.xlabel('Metai')
+    plt.ylabel('Moters amžius')
+    plt.legend()
+    plt.show()
+
+def main():
+    file = 'gimd_mot_amzius.csv'
+    data = load_data1(file)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
