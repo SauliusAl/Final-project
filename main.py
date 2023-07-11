86eda26901ce9a748baeb1caf7045b1a85f6e88a
gitSaulius <saule777@gmail.com>
2023-07-11 14:17:30 +0300

    Baigiamojo atnaujinimas

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from colorama import init, Fore, Back, Style
 
-def load_data1(file):
-    data1 = pd.read_csv(file, encoding='utf-8')
+def load_data(file):
+    data = pd.read_csv(file, encoding='utf-8')
     # return data
 
-    list_of_years1 = data1['Metai'].to_list()
-    list_of_vma = data1['Vid_mot_amzius'].to_list()
-    list_of_vap = data1['Vid_amz_pirmag'].to_list()
+    list_of_years = data['metai'].to_list()
+    list_of_vma = data['vid_mot_amz'].to_list()
+    list_of_vap = data['vid_amz_pirmag'].to_list()
 
-    x = np.array(list_of_years1)
+    x = np.array(list_of_years)
     y1 = np.array(list_of_vma)
     y2 = np.array(list_of_vap)
 
@@ -26,9 +26,8 @@ def load_data1(file):
     plt.show()
 
 def main():
-    file = 'gimd_mot_amzius.csv'
-    data = load_data1(file)
-
+    file = 'gimst_mirstam.csv'
+    data = load_data(file)
 
 if __name__ == '__main__':
     main()
